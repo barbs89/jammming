3f872261c327f787ef2fc56df81ffa5618c64205
Barbara Rodriguez <barbs2089@gmail.com>
2022-06-16 21:55:41 +1000

    Add import in App to reference static components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,20 @@
 import './App.css';
 
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+      <div class="App">
+        <SearchBar />
+        <div class="App-playlist">
+          <SearchResults />
+          <Playlist />
+        </div>
+      </div>
     </div>
   );
 }
