b1e876aba7d9b6452bf26c803ee666aef2063fc8
Barbara Rodriguez <barbs2089@gmail.com>
2022-07-31 20:30:20 +1000

    Add method to save the playlist to user's account

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,11 +2,29 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			term: ''
+		}
+
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.search = this.search.bind(this);
+	}
+
+	handleTermChange({ target }) {
+		this.setState({ term: target.value })
+	}
+
+	search() {
+		this.props.onSearch(this.state.term);
+	}
 
 	render() {
 		return (
 			<div className="SearchBar">
-				<input placeholder="Enter A Song, Album, or Artist" />
+				<input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
 				<button className="SearchButton">SEARCH</button>
 			</div>
 			)
