97e1add175532299a48f783a3e3d023950932715
Barbara Rodriguez <barbs2089@gmail.com>
2022-08-07 06:12:50 +1000

    Add Spotify search result request method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -8,7 +8,6 @@ const Spotify = {
 		if (accessToken) {
 			return accessToken;
 		}
-		
 		//check for access token match
 		const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
 		const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
@@ -25,6 +24,25 @@ const Spotify = {
 			const accessUrl = 	`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
 			window.location = accessUrl;
 		}
+	},
+	async search(term) {
+		const accessToken = Spotify.getAccessToken();
+		const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+			headers: {
+				Authorization: `Bearer ${accessToken}`
+			}
+		});
+		const jsonResponse = await response.json();
+		if (!jsonResponse.tracks) {
+			return [];
+		}
+		return jsonResponse.tracks.items.map(track => ({
+			id: track.id,
+			name: track.name,
+			artist: track.artists[0].name,
+			album: track.album.name,
+			URI: track.uri
+		}));
 	}
 };
 
